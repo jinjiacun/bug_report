5cc9d23f6c207839466b519e404efb077162528f
jim <jinjiacuncucu@sina.com>
2015-11-13 11:11:19 +0800

    modified:   jimLib/widget/MulCheckedBox.pyc 	modified:   jimLib/widget/TableComButton.pyc 	modified:   jimLib/widget/TableTextButton.py 	modified:   jimLib/widget/TableTextButton.pyc

--- jimLib/widget/TableTextButton.py
+++ jimLib/widget/TableTextButton.py
@@ -30,43 +30,83 @@ class TableTextButton(QtGui.QWidget):
         self.editText = QtGui.QLineEdit()
         self.editText.setFixedWidth(150)
         girdLayout.addWidget(self.editText,1,0)
+        #self.connect(self.editText,QtCore.SIGNAL("returnPressed()"),self.editChange)
+        self.editText.textChanged.connect(self.editChange)
         #按钮
-        btnAdd = QtGui.QPushButton('+')
-        btnAdd.clicked.connect(self.addText)
-        btnAdd.setFixedWidth(20)
-        girdLayout.addWidget ( btnAdd , 1, 1)
-        btnDel = QtGui.QPushButton('-')
-        btnDel.clicked.connect(self.delText)
-        btnDel.setFixedWidth(20)
-        girdLayout.addWidget(btnDel,1,2)
+        self.btnAdd = QtGui.QPushButton('+')
+        self.btnAdd.clicked.connect(self.addText)
+        self.btnAdd.setFixedWidth(20)
+        self.btnAdd.setEnabled(False)
+        girdLayout.addWidget ( self.btnAdd , 1, 1)
+        self.btnDel = QtGui.QPushButton('-')
+        self.btnDel.clicked.connect(self.delText)
+        self.btnDel.setFixedWidth(20)
+        self.btnDel.setEnabled(False)
+        girdLayout.addWidget(self.btnDel,1,2)
+
+        #测试
+        '''
+        self.btnTest = QtGui.QPushButton(u'测试')
+        self.btnTest.clicked.connect(self.test)
+        girdLayout.addWidget(self.btnTest,1,3)
+        '''
+
+    def editChange(self):
+        if '' == self.editText.text():
+            self.btnAdd.setEnabled(False)
+        else:
+            self.btnAdd.setEnabled(True)
+        if 0< self.list.rowCount():
+            self.btnDel.setEnabled(True)
+        else:
+            self.btnDel.setEnabled(False)
+        pass
 
     def addText(self):
+        if '' == self.editText.text():
+            return
         self.data.append(str(self.editText.text()))
         #去重排序
         my_data = sorted(set(self.data),key=self.data.index)
-        TableTextButton.data = my_data
+        self.data = my_data
 
         #设置行数
         self.list.setRowCount(len(self.data))
         row = 0
-        for item_text in TableTextButton.data:
+        for item_text in self.data:
             newItem = QtGui.QTableWidgetItem(item_text)
             newItem.setTextAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
             self.list.setItem(row, 0, newItem)
-
             row += 1
 
         self.editText.setText('')
+        if 0< self.list.rowCount():
+            self.btnDel.setEnabled(True)
+        else:
+            self.btnDel.setEnabled(False)
+
 
     def delText(self):
-        item = self.list.item(self.list.currentRow(),0)
+        if -1 == self.list.currentRow():
+            item = self.list.item(0,0)
+        else:
+            item = self.list.item(self.list.currentRow(),0)
         self.data.remove(item.text())
         #移除数据
-        self.list.removeRow(self.list.currentRow())
-        pass
+        if -1 == self.list.currentRow():
+            self.list.removeRow(0)
+        else:
+            self.list.removeRow(self.list.currentRow())
+        if 0< self.list.rowCount():
+            self.btnDel.setEnabled(True)
+        else:
+            self.btnDel.setEnabled(False)
 
     def text(self):
         return self.data
+
+    def test(self):
+        print self.text()
 
 if __name__ == '__main__':
     app = QtGui.QApplication([])
